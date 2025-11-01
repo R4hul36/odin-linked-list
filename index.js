13b2ac7c4a31669622f6cd1b32c5f6f667c67c4a
Rahul <rahul.wa47@gmail.com>
2025-11-01 12:49:40 +0530

    size, head, tail, at(index) and toString methods are done

--- index.js
+++ index.js
@@ -1,53 +1,98 @@
-
 class Node {
-    constructor(data, next) {
-        this.data = data;
-        this.next = next
-    }
+  constructor(data, next) {
+    this.data = data
+    this.next = next
+  }
 }
-
 
 class Linkedlist {
-    constructor(head, tail) {
-        this.head = null;
-        this.tail = null;
+  constructor(head, tail) {
+    this.head = null
+    this.tail = null
+  }
+
+  prepend(data) {
+    if (this.head) {
+      let nd = new Node(data, this.head)
+      this.head = nd
+    } else {
+      this.head = new Node(data, null)
+    }
+  }
+
+  append(data) {
+    if (this.head) {
+      let currNode = this.head
+
+      while (currNode.next !== null) {
+        currNode = currNode.next
+      }
+      currNode.next = new Node(data, null)
+    } else {
+      this.head = new Node(data, null)
+    }
+  }
+
+  size() {
+    let size = 0
+
+    let currNode = this.head
+    if (!currNode) {
+      return size
     }
 
-    prepend(data) {
-        if(this.head) {
-            let nd = new Node(data, this.head)
-            this.head = nd;
-        }else {
-            this.head = new Node(data, null)
-        }
+    while (currNode.next !== null) {
+      size += 1
+      currNode = currNode.next
     }
+    return size + 1
+  }
 
-    append(data) {
-        if(this.head) {
-            let currNode = this.head
+  getHead() {
+    return this.head
+  }
 
-            while(currNode.next !== null) {
-                console.log("end reached");
-                currNode = currNode.next
-            }
-            currNode.next = new Node(data, null)
-            
-        }else {
-            this.head = new Node(data, null)
-        }
+  getTail() {
+    let currNode = this.head
+
+    while (currNode.next !== null) {
+      currNode = currNode.next
     }
+    return currNode
+  }
+
+  nodeAt(index) {
+    let currNode = this.head
+    let currIndex = 0
+
+    while (index > currIndex) {
+      currNode = currNode.next
+      currIndex++
+    }
+    return currNode
+  }
+
+  listToString() {
+    let currNode = this.head
+
+    let listString = ''
+
+    while (currNode.next !== null) {
+      listString += `(${currNode.data}) -> `
+      currNode = currNode.next
+    }
+    return `${listString} (${currNode.data})`
+  }
 }
 
+const list = new Linkedlist()
 
-const list = new Linkedlist();
+list.prepend('one')
+list.prepend('two')
+list.prepend('three')
+list.append('jilly')
+list.append('joe')
 
-// list.prepend("one")
-list.prepend("two")
-list.prepend("three")
-list.append("jilly")
-list.append("joe")
+list.append('sdfdsf')
 
- list.append("sdfdsf")
-
-
-console.log(list);
+console.log(list.listToString())
